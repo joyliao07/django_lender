8594c7da6e442fb4e301704077145f9cd3260ea9
Joyce Liao <joyliao07@gmail.com>
2019-02-12 21:34:28 -0800

    install channels

--- django_lender/asgi.py
+++ django_lender/asgi.py
@@ -3,15 +3,11 @@ ASGI entrypoint. Configures Django and then runs the application
 defined in the ASGI_APPLICATION setting.
 """
 
-# import os
-# import django
-# from django.core.wsgi import get_wsgi_application
-# from channels.routing import get_default_application
+import os
+import django
+from django.core.wsgi import get_wsgi_application
+from channels.routing import get_default_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_lender.settings')
-# django.setup()
-# application = get_default_application()
-
-# Procfile:
-# web: daphne -b 0.0.0.0 -p 8000 django_lender.asgi:application
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_lender.settings')
+django.setup()
+application = get_default_application()
--- django_lender/settings.py
+++ django_lender/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_registration',
     'whitenoise.runserver_nostatic',
+    'channels',
 ]
 
 MIDDLEWARE = [
