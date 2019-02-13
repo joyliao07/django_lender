9e949ef1f8240cdf3f3d524f7af2bdb66d61c17f
Joyce Liao <joyliao07@gmail.com>
2019-02-12 21:11:50 -0800

    Procfile config

---
+++ django_lender/asgi.py
@@ -0,0 +1,13 @@
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+import django
+from django.core.wsgi import get_wsgi_application
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_lender.settings')
+django.setup()
+application = get_default_application()
