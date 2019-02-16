b4f75be60410dfe1b4514d900de0dcdcff6b7443
Joyce Liao <joyliao07@gmail.com>
2019-02-16 15:11:37 -0800

    modify manage.py

--- django_lender/settings.py
+++ django_lender/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '5zeq@n@ou#oixtvix1gta$^r)mw-@b1(ymn1f59j830d5b5e#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
